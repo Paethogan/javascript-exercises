0c01cea15f2ed2ec0387be14ba0bd0335a4fcec5
Paethogan <Shadowsparrow70@gmail.com>
2025-03-13 01:47:38 -0500

    02_repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let myString = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let count = 1; count <= num; count++) {
+        myString += str;
+    }
+    return myString;
 };
 
 // Do not edit below this line
