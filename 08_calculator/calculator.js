0090ba52e0014e7bf4f2c662ce43892a2a791185
Paethogan <Shadowsparrow70@gmail.com>
2025-03-25 05:14:37 -0500

    08_calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(...num) {
+  return num.reduce((sum, num) => sum + num);
 };
 
-const subtract = function() {
-	
+const subtract = function(...num) {
+  return num.reduce((total, num) => total - num);
 };
 
-const sum = function() {
-	
+const sum = function(arr = []) {
+  return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr = []) {
+  return arr.reduce((product, num) => product * num);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  return num <= 1 ? 1 : num * factorial(num - 1);
 };
 
 // Do not edit below this line
