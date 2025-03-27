77a96ee671ddabbcb1959b9fdb664bf1c85e41d5
Paethogan <Shadowsparrow70@gmail.com>
2025-03-27 04:12:55 -0500

    10_fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let num = parseInt(n);
+    if (num < 0) {
+        return 'OOPS';
+    }
+    if (num <= 1) return num;
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
