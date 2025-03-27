f6f73b325c1367663f7b3bfcafa8b589342b431c
Paethogan <Shadowsparrow70@gmail.com>
2025-03-27 03:53:09 -0500

    09_palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let unstrict = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let palindrome = unstrict.split('').reverse().join(''); 
+    if (palindrome === unstrict) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
